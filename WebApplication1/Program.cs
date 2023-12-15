549db52cd4b23d9a27bd9d2303c0ede311f6f1fa
vladosss5 <kiporovv1@gmail.com>
2023-12-15 23:31:45 +0700

    dev\prod

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -7,17 +7,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string environment = builder.Configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT") ?? "Production"; 
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<MyDbContext>(options => options.UseNpgsql(connection));
 
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+if (environment == "Development")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (environment == "Production")
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseHttpsRedirection();
 
